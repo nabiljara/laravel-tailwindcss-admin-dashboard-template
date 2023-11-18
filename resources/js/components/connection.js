97cafc6b6ba281e603dfd1020df10880a3c09d40
DESKTOP-GDMDAN5\Nabil <nabiljara26@gmail.com>
2023-11-18 10:32:28 -0300

    Export connection

--- resources/js/components/connection.js
+++ resources/js/components/connection.js
@@ -2,30 +2,31 @@ import mqtt from 'mqtt'
 // const mqtt = require('mqtt')
 const connection = () =>{
 
-const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
+const clientId = `pagina_web_clima`
 const connectUrl = `ws://150.230.80.1:8083/mqtt`
 const options = {
   clientId,
-  clean: true,
+  clean: false,
   connectTimeout: 4000,
   username: 'Nabil',
   password: '1234',
-  reconnectPeriod: 10000,
-  keepalive: 60,
+  reconnectPeriod: 1000,
+  keepalive: 0,
 }
-const topic = "test";
+// const topic = "Estación-145839-Sensor-557448/bar-absolute";
 const client = mqtt.connect(connectUrl, options)
       client.on('connect', () => {
         console.log("Connected!")
       })
-      client.subscribe(topic, () => {
-        console.log(`Subscribe to topic '${topic}'`)
-      })
-      client.on('message', (topic, payload) => {
-        //insert on bd mongo
-        let strMessage = payload.toString();
-        let objMessage = JSON.parse(strMessage);
-        console.log(objMessage.msg)
-      });
+      // client.subscribe(topic, () => {
+      //   console.log(`Subscribe to topic '${topic}'`)
+      // })
+      // client.on('message', (topic, payload) => {
+      //   //insert on bd mongo
+      //   let strMessage = payload.toString();
+      //   let objMessage = JSON.parse(strMessage);
+      //   console.log(strMessage)
+      // });
+      return client;
     }
 export default connection;
--- resources/views/pages/dashboard/dashboard.blade.php
+++ resources/views/pages/dashboard/dashboard.blade.php
@@ -38,9 +38,9 @@
                         <li class="me-2" role="presentation">
                             <button
                                 class="station inline-block rounded-t-lg border-b-2 border-transparent p-4 hover:border-gray-300 hover:text-gray-600 dark:hover:text-gray-300"
-                                id="{{$station['station_id_uuid']}}-tab" type="button" role="tab" aria-controls="{{$station['station_id_uuid']}}"
-                                aria-selected="false">
-                                {{$station['station_name']}}
+                                id="{{ $station['station_name'] }}" type="button" role="tab"
+                                aria-controls="{{ $station['station_id_uuid'] }}" aria-selected="false">
+                                {{ $station['station_name'] }}
                             </button>
                         </li>
                     @endforeach
@@ -50,20 +50,65 @@
         </div>
         <div id="tabContentExample">
             @foreach ($stations as $station)
-                
-            <div class="station-target hidden rounded-lg bg-gray-50 p-4 dark:bg-gray-800" id="{{$station['station_id_uuid']}}" role="tabpanel"
-            aria-labelledby="{{$station['station_id_uuid']}}-tab">
-            {{-- Acá van todos los elementos para mostrar --}}
-            <p class="text-sm text-gray-500 dark:text-gray-400">
-                This is some placeholder content the
-                <strong class="font-medium text-gray-800 dark:text-white">{{$station['station_name']}} tab's associated
-                    content</strong>
-                </p>
-            </div>
+                {{-- <div class="station-target hidden rounded-lg bg-gray-50 p-4 dark:bg-gray-800"> --}}
+                {{-- Acá van todos los elementos para mostrar --}}
+                <div class="station-target hidden grid grid-cols-12 gap-4" id="{{ $station['station_id_uuid'] }}" role="tabpanel"
+                    aria-labelledby="{{ $station['station_name'] }}">
+                    {{-- Componentes --}}
+                    <div class="col-span-12 sm:col-span-4">
+                        <div class="p-4 relative  bg-gray-800 border border-gray-800 shadow-lg  rounded-2xl">
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                class="h-14 w-14  absolute bottom-4 right-3 text-green-400" viewBox="0 0 20 20"
+                                fill="currentColor">
+                                <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z" />
+                                <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z" />
+                            </svg>
+                            <div class="flex justify-between items-center ">
+                                <img class="w-7 filter grayscale"
+                                    src="https://v1.tailwindcss.com/_next/static/media/tailwindcss-mark.6ea76c3b72656960a6ae5ad8b85928d0.svg"
+                                    alt="taiwind css">
+                            </div>
+                            <div class="measure text-2xl text-gray-100 font-medium leading-8 mt-5">20</div>
+                            <div class="text-sm text-gray-500">{{ $station['station_name'] }}</div>
+                        </div>
+                    </div>
+                    <div class="col-span-12 sm:col-span-4">
+                        <div class="p-4 relative  bg-gray-800 border border-gray-800 shadow-lg  rounded-2xl">
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                class="h-14 w-14  absolute bottom-4 right-3 text-blue-500" viewBox="0 0 20 20"
+                                fill="currentColor">
+                                <path
+                                    d="M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z" />
+                            </svg>
+                            <div class="flex justify-between items-center ">
+                                <i class="fab fa-behance text-xl text-gray-400"></i>
+                            </div>
+                            <div class="text-2xl text-gray-100 font-medium leading-8 mt-5">99</div>
+                            <div class="text-sm text-gray-500">Projects</div>
+                        </div>
+                    </div>
+                    <div class="col-span-12 sm:col-span-4">
+                        <div class="p-4 relative  bg-gray-800 border border-gray-800 shadow-lg  rounded-2xl">
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                class="h-14 w-14  absolute bottom-4 right-3 text-yellow-300" viewBox="0 0 20 20"
+                                fill="currentColor">
+                                <path fill-rule="evenodd"
+                                    d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                            <div class="flex justify-between items-center ">
+                                <i class="fab fa-codepen text-xl text-gray-400"></i>
+                            </div>
+                            <div class="text-2xl text-gray-100 font-medium leading-8 mt-5">50</div>
+                            <div class="text-sm text-gray-500">Pen Items</div>
+                        </div>
+                    </div>
+                </div>
+                {{-- </div> --}}
             @endforeach
         </div>
 
-        <div class="grid grid-cols-12 gap-4 ">
+        {{-- <div class="grid grid-cols-12 gap-4 ">
             <div class="col-span-12 sm:col-span-4">
                 <div class="p-4 relative  bg-gray-800 border border-gray-800 shadow-lg  rounded-2xl">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-14 w-14  absolute bottom-4 right-3 text-green-400"
@@ -109,7 +154,7 @@
                     <div class="text-sm text-gray-500">Pen Items</div>
                 </div>
             </div>
-        </div>
+        </div> --}}
         <br><br><br>
         <div class="grid gap-4 grid-cols-1 md:grid-cols-2">
 
