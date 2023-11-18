2c62d4399f8a652cb6c2424957e4c73ded7f7db8
Martín Alvarez <alvarezlmartin@gmail.com>
2023-11-18 17:54:37 -0300

    Reglas en dashboard

--- app/Http/Livewire/CrearRegla.php
+++ app/Http/Livewire/CrearRegla.php
@@ -8,29 +8,7 @@ use App\Models\Regla;
 class CrearRegla extends Component
 {
 
-    public $temperaturaMax = '';
-    public $temperaturaMin = '';
-
-    public $vientoMax = '';
-    public $vientoMin = '';
-
-    public $humedadMax = '';
-    public $humedadMin = '';
-
-    public $rocioMax = '';
-    public $rocioMin = '';
-
-
-    public function save()
-    {
-        $this->validate();
-
-        Regla::create(
-            $this->only(['temperaturaMax', 'temperaturaMin', 'vientoMax', 'vientoMin', 'humedadMax', 'humedadMin', 'rocioMax', 'rocioMin'])
-        );
-
-        return $this->redirect('/crear-regla');
-    }
+    public $opcionSeleccionada = false;
 
     public function render()
     {
